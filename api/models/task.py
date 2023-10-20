0d712fa5bba6bd6d0e30baaa912bc9c3af58671e
keisho <t120045@ed.sus.ac.jp>
2023-10-20 17:33:49 +0900

    modelsのtaskの変更(done等->status)

--- api/models/task.py
+++ api/models/task.py
@@ -4,40 +4,58 @@ from sqlalchemy.orm import relationship
 from api.db import Base
 from datetime import date
 
+class User(Base):
+    __tablename__ = "users"
+    
+    user_id = Column(Integer, nullable=False, primary_key=True)
+    user_name = Column(String(1024), nullable=False)
+    
+    task = relationship("Task", back_populates="user", cascade="delete")
 
 class Task(Base):
     __tablename__ = "tasks"
 
+    user_id = Column(Integer, ForeignKey("users.user_id"), primary_key=True)
+    
     id = Column(Integer, primary_key=True)
     title = Column(String(1024))
     start_expected_date = Column(DATETIME)
     start_date = Column(DATETIME)
     end_expected_date = Column(DATETIME)
     end_date = Column(DATETIME)
+    status = Column(Integer)
     
-    before_work = relationship("Before_work", back_populates="task", cascade="delete")
-    in_work = relationship("In_work", back_populates="task", cascade="delete")
-    done = relationship("Done", back_populates="task", cascade="delete")
+    user = relationship("User", back_populates="task")
+    # status = relationship("Status", back_populates="task", cascade="delete")
+
+
+# class Status(Base):
+#     __tablename__ = "status"
+    
+#     todo = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
+#     doing = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
+#     done = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
+    
+#     task = relationship("Task", back_populates="status")
     
 
+# class Todo(Base):
+#     __tablename__ = "todo"
 
-class Before_work(Base):
-    __tablename__ = "before_work"
+#     id = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
 
-    id = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
+#     task = relationship("Task", back_populates="todo")
 
-    task = relationship("Task", back_populates="before_work")
+# class Doing(Base):
+#     __tablename__ = "doing"
 
-class In_work(Base):
-    __tablename__ = "in_work"
+#     id = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
 
-    id = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
+#     task = relationship("Task", back_populates="doing")
 
-    task = relationship("Task", back_populates="in_work")
+# class Done(Base):
+#     __tablename__ = "dones"
 
-class Done(Base):
-    __tablename__ = "dones"
+#     id = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
 
-    id = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
-
-    task = relationship("Task", back_populates="done")
+#     task = relationship("Task", back_populates="done")
